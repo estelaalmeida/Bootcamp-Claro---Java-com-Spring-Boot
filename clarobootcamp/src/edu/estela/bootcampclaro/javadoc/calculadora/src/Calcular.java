9ab06271e46c5a619e5b5bd92606e93590e87e30
estelaalmeida <estela_almeida.oliveira@hotmail.com>
2025-03-05 14:10:50 -0300

    Aprendendo a Sintaxe java

---
+++ clarobootcamp/src/edu/estela/bootcampclaro/javadoc/calculadora/src/Calcular.java
@@ -0,0 +1,14 @@
+
+//*@author Estela
+
+public class Calcular {
+   public static void main(String[] args) {
+    int umNumero = 50;
+    int numero = 25;
+    int soma = umNumero + numero; 
+
+    System.out.printf("soma: " + soma / numero);
+   }
+         
+    }
+
